1da9bbc2f1be5135efa243ec1cfb217603ac85b0
karnarajbanshi <67020608+karnarajbanshi@users.noreply.github.com>
2022-12-24 14:56:27 +0545

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Transactions;
+
+public class ConsoleApp
+{
+    public static int choice;
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("---------------x-------------xx-------------x-----------------------------x-------------xx-------------x--------------");
+        Console.WriteLine("1.Program to demonstrate the operators\t\t\t" + "2.Program to demostrate control statements\n" + "3.Program to demostrate Array         \t\t\t" + "4.Program to demostrate OOP\n" + "5.Program to demostrate Polymorphism\t\t\t" + "6.Program to demostrate Inheritence\n" + "7.Program to demonstrate Encapsulation\t\t\t" + "8.Program to demonstrate Abstraction\n" + "9.Program to demostrate constructor\t\t\t" + "10.Program to demostrate the delegates\n" + "11.Program to demonstrate exception handling\t\t\t" + "12.\n" + "13.\t\t\t" + "14.\n" + "15.\t\t\t" + "16.\n" + "17.\t\t\t" + "18.\n" + "19.\t\t\t" + "20.\n" + "21.\t\t\t" + "22.\n" + "23.\t\t\t" + "24.\n" + "25.\t\t\t" + "26.\n" + "27.\t\t\t");
+        Console.WriteLine("---------------x-------------xx-------------x-----------------------------x-------------xx-------------x--------------");
+        Console.WriteLine("Enter your choice");
+        var input = Console.ReadLine();
+        choice = Convert.ToInt32(input);
+        // Program control
+        switch (choice)
+        {
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+            case 1:
+                {
+
+                }
+        }
+
+
+    }
+}
